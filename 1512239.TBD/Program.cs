e1a936af010ec379cc38d1aa224868a3923d4c5b
Vu Thua Khang <vuthuakhangit@gmail.com>
2018-11-25 16:28:49 +0700

    Add project files.

---
+++ 1512239.TBD/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1512239.TBD
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+#endif
+}
