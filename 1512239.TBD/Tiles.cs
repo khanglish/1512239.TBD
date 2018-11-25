e86c7ff360cab15597f009ad41a6f675759e05f7
Vu Thua Khang <vuthuakhangit@gmail.com>
2018-11-25 16:36:30 +0700

    added Tiles and CollisionTiles

---
+++ 1512239.TBD/Tiles.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace _1512239.TBD
+{
+    class Tiles
+    {
+        protected Texture2D texture;
+
+        private Rectangle rectangle;
+
+        public  Rectangle Rectangle
+        {
+            get { return rectangle; }
+            protected set => rectangle = value;
+        }
+
+        
+        private static ContentManager content;
+        public static ContentManager Content
+        {
+            protected get { return content; }
+            set { content = value;  }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, rectangle, Color.White);
+        }
+    }
+
+    class CollisionTiles : Tiles
+    {
+        public CollisionTiles(int i , Rectangle newRectangle)
+        {
+            texture = Content.Load<Texture2D>("Tile" + i);
+            this.Rectangle = newRectangle;
+        }
+    }
+}
